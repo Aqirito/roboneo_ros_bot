5bba75d30d688b5fc52405c174652a8eb49e0be2
Aqirito <aqil.rahmat99@gmail.com>
2025-09-28 02:35:37 +0800

    UPDATE: config

--- roboneo_bot/config.py
+++ roboneo_bot/config.py
@@ -3,8 +3,8 @@ def load_config():
         'events_url'           : 'http://139.162.41.238:1880/events', # will be provided by event Instructor
         'secret_key'           : 'D4WS34NNS57M', # will be provided by event Instructor. Dont share with anyone
         'robot_id'             : 'kinabalu_coders', # will be provided by event Instructor
-        'detected_distance'    : 18.0, # distance to consider as obstacle
-        'forward_speed'        : 0.6,  # Forward speed
+        'detected_distance'    : 20.0, # distance to consider as obstacle
+        'forward_speed'        : 0.7,  # Forward speed
         'turn_90deg_duration'  : 0.7,  # seconds to turn ~90 degrees
         'turn_180deg_duration' : 1.45,  # seconds to turn ~180 degrees example turn from right to left
         'pillar_scan_duration' : 2.7,  # seconds for 360° rotation
